fc95e105ac830feb45e11aeb1b4df41633006464
Carlos Echeverria <echeverriadev@gmail.com>
2020-07-16 22:33:51 -0400

    hardcoding

--- config/enviroments.js
+++ config/enviroments.js
@@ -3,23 +3,25 @@
 // ============================
 
 process.env.NODE_PORT = process.env.NODE_PORT || 3000;
-process.env.REACT_PORT = process.env.REACT_PORT; // || "http://localhost:3001";
-process.env.NODE_URL = process.env.NODE_URL; // || "http://localhost:3000";
+process.env.REACT_PORT = process.env.REACT_PORT || 'https://coacademy.cl';
+process.env.NODE_URL =
+  process.env.NODE_URL || 'https://coacademy-server-jc.com';
 
 // ============================
 //  Data Base
 // ============================
 
-process.env.DB_HOST = process.env.DB_HOST; // || "localhost";
-process.env.DB_USER = process.env.DB_USER; // || "echeverriadev";
-process.env.DB_PASS = process.env.DB_PASS; // || "dev_pass";
-process.env.DB_NAME = process.env.DB_NAME; // || "course_db";
+process.env.DB_HOST =
+  process.env.DB_HOST || 'course-db.cgxnznbxddwm.us-west-1.rds.amazonaws.com';
+process.env.DB_USER = process.env.DB_USER || 'adminDBCoacademy';
+process.env.DB_PASS = process.env.DB_PASS || 'y2EOuYwUvk6uhU6Ye8iW';
+process.env.DB_NAME = process.env.DB_NAME || 'coacademy';
 
 // ============================
 //  ENVIROMENT
 // ============================
 
-process.env.NODE_ENV = process.env.NODE_ENV; // || "dev";
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // ============================
 //  Token Expiration
@@ -35,4 +37,4 @@ process.env.EXPIRATION_TOKEN = 60 * 60 * 24 * 30;
 //  AUTENTHICATION SEED
 // ============================
 
-process.env.SEED = process.env.SEED; // || "development-seed";
+process.env.SEED = process.env.SEED || 'development-seed';
